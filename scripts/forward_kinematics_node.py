1b7262c6c8cce467ef5c2679dc4afa8efd73a166
Masa0u0 <masa0u0masa1215@gmail.com>
2023-12-02 15:39:07 +0900

    .

--- scripts/forward_kinematics_node.py
+++ scripts/forward_kinematics_node.py
@@ -16,11 +16,13 @@ if __name__ == "__main__":
     link_name = rospy.get_param("~link_name")
 
     frame = tree.global_pose(link_name)
-    pos = frame.p
+    x = frame.p.x
+    y = frame.p.y
+    z = frame.p.z
     roll, pitch, yaw = frame.M.get_rpy()
-    print(f"X    : {sympy.simplify(pos.x, chop=True)}")
-    print(f"Y    : {sympy.simplify(pos.y, chop=True)}")
-    print(f"Z    : {sympy.simplify(pos.z, chop=True)}")
+    print(f"X    : {sympy.simplify(x, chop=True)}")  # TODO: chopが機能しない
+    print(f"Y    : {sympy.simplify(y, chop=True)}")
+    print(f"Z    : {sympy.simplify(z, chop=True)}")
     print(f"Roll : {sympy.simplify(roll, chop=True)}")
     print(f"Pitch: {sympy.simplify(pitch, chop=True)}")
     print(f"Yaw  : {sympy.simplify(yaw, chop=True)}")
